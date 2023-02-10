b11ea099d4ef8ce02d2bef5fe76507a922e2b7dc
Maxuell Lima <71743257+Maxuell123@users.noreply.github.com>
2023-02-10 13:12:20 -0300

    New

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// NAVBAR VARIABLES
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const header = document.querySelector("[data-header]");
+
+navToggleBtn.addEventListener("click", function () {
+   header.classList.toggle("active");
+});
